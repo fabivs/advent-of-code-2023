03ea0b15c094450458260755d72221a74f529f2f
Fabio Prizio <fabio.prizio@casavo.com>
2023-12-03 20:58:39 +0100

    day 1 first and second problems solutions

---
+++ lib/day1.ex
@@ -0,0 +1,86 @@
+defmodule Day1 do
+  @moduledoc """
+  https://adventofcode.com/2023/day/1
+  """
+
+  @numbers %{
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9"
+  }
+
+  def find_calibration_result(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      first_value =
+        line
+        |> String.codepoints()
+        |> Enum.find(&is_codepoint_a_number?/1)
+
+      last_value =
+        line
+        |> String.codepoints()
+        |> Enum.reverse()
+        |> Enum.find(&is_codepoint_a_number?/1)
+
+      Enum.join([first_value, last_value]) |> String.to_integer()
+    end)
+    |> Enum.reduce(0, &Kernel.+(&1, &2))
+  end
+
+  def find_calibration_result_improved(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line -> first_and_last_numbers_of_string_as_number(line) end)
+    |> Enum.reduce(0, &Kernel.+(&1, &2))
+  end
+
+  defp first_and_last_numbers_of_string_as_number(line) do
+    numbers_as_text_patterns = Map.keys(@numbers)
+
+    first_value =
+      line
+      |> String.replace(numbers_as_text_patterns, fn matching_pattern ->
+        Map.get(@numbers, matching_pattern, matching_pattern)
+      end)
+      |> get_first_number_of_string()
+
+    reversed_line = String.reverse(line)
+    reversed_numbers_as_patterns = Enum.map(numbers_as_text_patterns, &String.reverse/1)
+
+    last_value =
+      reversed_line
+      |> String.replace(reversed_numbers_as_patterns, fn matching_pattern ->
+        restored_pattern = String.reverse(matching_pattern)
+        Map.get(@numbers, restored_pattern, matching_pattern)
+      end)
+      |> get_first_number_of_string()
+      |> String.reverse()
+
+    Enum.join([first_value, last_value]) |> String.to_integer()
+  end
+
+  defp get_first_number_of_string(string) do
+    string
+    |> String.codepoints()
+    |> Enum.find(&is_codepoint_a_number?/1)
+  end
+
+  defp is_codepoint_a_number?(codepoint) do
+    try do
+      _ = String.to_integer(codepoint)
+      true
+    rescue
+      _error -> false
+    end
+  end
+end
---
+++ test/day1_test.exs
@@ -0,0 +1,38 @@
+defmodule Day1Test do
+  use ExUnit.Case
+
+  test "compute the calibration number" do
+    example_input = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
+    example_result = 142
+    assert example_result == Day1.find_calibration_result(example_input)
+
+    {:ok, solution_input} = File.read("day1_input")
+    solution_result = 54968
+    assert solution_result == Day1.find_calibration_result(solution_input)
+  end
+
+  test "compute the calibration number with the improved version" do
+    example_input = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+    example_result = 281
+    assert example_result == Day1.find_calibration_result_improved(example_input)
+
+    {:ok, solution_input} = File.read("day1_input")
+    solution_result = 54094
+    assert solution_result == Day1.find_calibration_result_improved(solution_input)
+  end
+end
