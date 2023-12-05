1e8e301c227bee31216141431ec719e0e5f72d7f
Fabio Prizio <fabio.prizio@casavo.com>
2023-12-05 14:32:41 +0100

    day 2 part 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -3,78 +3,122 @@ defmodule Day2 do
   https://adventofcode.com/2023/day/2
   """
 
-  @spec id_sum_of_possible_games(binary(), map()) :: integer()
-  def id_sum_of_possible_games(results, bag) do
-    find_possible_games(results, bag)
-    |> Enum.reduce(0, fn {game_id, possible?}, acc ->
-      if possible? do
-        acc + game_id
-      else
-        acc
-      end
-    end)
+  defmodule Part1 do
+    @spec id_sum_of_possible_games(binary(), map()) :: integer()
+    def id_sum_of_possible_games(results, bag) do
+      possible_games(results, bag)
+      |> Enum.reduce(0, fn {game_id, possible?}, acc ->
+        if possible? do
+          acc + game_id
+        else
+          acc
+        end
+      end)
+    end
+
+    @spec possible_games(binary(), map()) :: [{binary(), boolean()}]
+    defp possible_games(results, %{} = bag) do
+      colors = bag |> Map.keys() |> Enum.map(&Atom.to_string/1)
+
+      results
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(fn game ->
+        id = game_id(game)
+
+        results_per_color =
+          Enum.map(colors, fn color ->
+            {color, results_for_color(game, color)}
+          end)
+          |> Map.new()
+
+        # check every value against the bag
+        each_color_possible? =
+          Enum.map(colors, fn color ->
+            color_atom = String.to_existing_atom(color)
+            max = Map.get(bag, color_atom)
+
+            results_per_color
+            |> Map.get(color)
+            |> Enum.reduce(true, fn amount, acc -> amount <= max and acc end)
+          end)
+
+        # return game id and true or false
+        {id, Enum.reduce(each_color_possible?, true, fn value, acc -> value and acc end)}
+      end)
+    end
+
+    defp game_id(game), do: get_game_id_in_string(game)
+
+    defp get_game_id_in_string(game_string) do
+      game_title = game_string |> String.split(":") |> List.first()
+      [_, id_string] = game_title |> String.split(" ")
+      String.to_integer(id_string)
+    end
+
+    @spec results_for_color(binary(), binary()) :: [integer()]
+    def results_for_color(game, color) do
+      [_game_info, results] = String.split(game, ":")
+
+      results
+      |> String.split(";")
+      |> Enum.map(fn single_extraction ->
+        single_extraction
+        |> String.split(",")
+        |> Enum.find(&String.contains?(&1, color))
+        |> case do
+          nil -> 0
+          string -> get_initial_number_in_string(string)
+        end
+      end)
+    end
+
+    # Eg. " 20 red"
+    defp get_initial_number_in_string(string) do
+      [number, _color] = string |> String.trim() |> String.split(" ")
+      String.to_integer(number)
+    end
   end
 
-  @spec find_possible_games(binary(), map()) :: [{binary(), boolean()}]
-  def find_possible_games(results, %{} = bag) do
-    colors = bag |> Map.keys() |> Enum.map(&Atom.to_string/1)
+  defmodule Part2 do
+    import Part1
 
-    results
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(fn game ->
-      id = game_id(game)
+    @spec sum_of_power_of_games(binary()) :: integer()
+    def sum_of_power_of_games(results) do
+      results
+      |> String.trim()
+      |> String.split("\n")
+      # NOTE: game == line
+      |> Enum.map(fn game ->
+        game
+        |> game_results_per_color()
+        |> min_bag_size()
+        |> power_of_game()
+      end)
+      |> Enum.sum()
+    end
 
-      results_per_color =
-        Enum.map(colors, fn color ->
-          {color, results_for_color(game, color)}
-        end)
-        |> Map.new()
+    @spec game_results_per_color(binary()) :: %{binary() => integer()}
+    defp game_results_per_color(game) do
+      colors = ["red", "green", "blue"]
 
-      # check every value against the bag
-      each_color_possible? =
-        Enum.map(colors, fn color ->
-          color_atom = String.to_existing_atom(color)
-          max = Map.get(bag, color_atom)
+      Enum.map(colors, fn color ->
+        {color, results_for_color(game, color)}
+      end)
+      |> Map.new()
+    end
 
-          results_per_color
-          |> Map.get(color)
-          |> Enum.reduce(true, fn amount, acc -> amount <= max and acc end)
-        end)
+    defp min_bag_size(%{} = game_results_per_color) do
+      Enum.map(game_results_per_color, fn {color, values} ->
+        {color, Enum.max(values)}
+      end)
+      |> Map.new()
+    end
 
-      # return game id and true or false
-      {id, Enum.reduce(each_color_possible?, true, fn value, acc -> value and acc end)}
-    end)
-  end
-
-  defp game_id(game), do: get_game_id_in_string(game)
-
-  @spec results_for_color(binary(), binary()) :: [integer()]
-  defp results_for_color(game, color) do
-    [_game_info, results] = String.split(game, ":")
-
-    results
-    |> String.split(";")
-    |> Enum.map(fn single_extraction ->
-      single_extraction
-      |> String.split(",")
-      |> Enum.find(&String.contains?(&1, color))
-      |> case do
-        nil -> 0
-        string -> get_initial_number_in_string(string)
-      end
-    end)
-  end
-
-  defp get_game_id_in_string(game_string) do
-    game_title = game_string |> String.split(":") |> List.first()
-    [_, id_string] = game_title |> String.split(" ")
-    String.to_integer(id_string)
-  end
-
-  # Eg. " 20 red"
-  defp get_initial_number_in_string(string) do
-    [number, _color] = string |> String.trim() |> String.split(" ")
-    String.to_integer(number)
+    defp power_of_game(%{} = min_bag_size) do
+      Enum.reduce(min_bag_size, 1, fn {_color, amount_for_color}, acc ->
+        amount_for_color * acc
+      end)
+    end
   end
 end
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -1,15 +1,17 @@
 defmodule Day2Test do
   use ExUnit.Case
 
-  test "find the possible games" do
-    example_input = """
-    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-    """
+  @example_input """
+  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+  """
 
+  @solution_input File.read!("day2_input")
+
+  test "find the possible games" do
     example_bag = %{
       red: 12,
       green: 13,
@@ -17,9 +19,7 @@ defmodule Day2Test do
     }
 
     example_result = 8
-    assert example_result == Day2.id_sum_of_possible_games(example_input, example_bag)
-
-    {:ok, solution_input} = File.read("day2_input")
+    assert example_result == Day2.Part1.id_sum_of_possible_games(@example_input, example_bag)
 
     solution_bag = %{
       red: 12,
@@ -28,6 +28,14 @@ defmodule Day2Test do
     }
 
     solution_result = 2632
-    assert solution_result == Day2.id_sum_of_possible_games(solution_input, solution_bag)
+    assert solution_result == Day2.Part1.id_sum_of_possible_games(@solution_input, solution_bag)
+  end
+
+  test "find the combined power of all games" do
+    example_result = 2286
+    assert example_result == Day2.Part2.sum_of_power_of_games(@example_input)
+
+    solution_result = 69629
+    assert solution_result == Day2.Part2.sum_of_power_of_games(@solution_input)
   end
 end
